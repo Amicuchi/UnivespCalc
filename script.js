3b8ae1cce01b63bad970ab862fe91667523ccc85
Amicuchi <andersonamicuchi@gmail.com>
2022-12-17 01:22:36 -0300

    Corrigindo o problema da não inserção da nota

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const calculaMedia = document.querySelector("#calculaMedia");
 const resultado = document.querySelector(".resultado");
 
 // Notas das provas
-const s1 = parseFloat(document.getElementById("nota1").value) * 0.08;
-const s2 = parseFloat(document.getElementById("nota2").value)* 0.12;
-const s3 = parseFloat(document.getElementById("nota3").value) * 0.17;
-const s4 = parseFloat(document.getElementById("nota4").value) * 0.17;
-const s5 = parseFloat(document.getElementById("nota5").value) * 0.17;
-const s6 = parseFloat(document.getElementById("nota6").value) * 0.17;
-const s7 = parseFloat(document.getElementById("nota7").value) * 0.12;
-const prova = parseFloat(document.getElementById("notaProva").value) * 0.6;
+const s1 = parseFloat(document.getElementById("nota1").value) * 0.08 || 0;
+const s2 = parseFloat(document.getElementById("nota2").value)* 0.12 || 0;
+const s3 = parseFloat(document.getElementById("nota3").value) * 0.17 || 0;
+const s4 = parseFloat(document.getElementById("nota4").value) * 0.17 || 0;
+const s5 = parseFloat(document.getElementById("nota5").value) * 0.17 || 0;
+const s6 = parseFloat(document.getElementById("nota6").value) * 0.17 || 0;
+const s7 = parseFloat(document.getElementById("nota7").value) * 0.12 || 0;
+const prova = parseFloat(document.getElementById("notaProva").value) * 0.6 || 0;
 
 const mediaAvaliativas = (s1 + s2 + s3 + s4 + s5 + s6 + s7) * 0.4
 const mediaFinal = mediaAvaliativas + prova;
